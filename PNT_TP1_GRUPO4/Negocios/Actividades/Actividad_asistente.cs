b2f79f2bcda17832ab75e9a3d5fffc1cb2b0a0ee
Denise-E <denueichen@gmail.com>
2023-04-26 22:11:19 -0300

    Arreglando TP1 PN

---
+++ PNT_TP1_GRUPO4/Negocios/Actividades/Actividad_asistente.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP1_GRUPO4.Negocios.Entidades;
+using TP1_GRUPO4.Persistencia;
+
+namespace TP1_GRUPO4.Negocios.Actividades
+{
+    public class Actividad_asistente
+    {
+        Persistencia_asistente pers = new Persistencia_asistente();
+
+        public bool Create(Asistente p)
+        {
+            return pers.Create(p);
+        }
+
+        public Asistente Read(int id)
+        {
+            return pers.Read(id);
+        }
+
+        public List<Asistente> ReadAll()
+        {
+            return pers.ReadAll();
+        }
+
+        public bool Update(Asistente a)
+        {
+            return pers.Update(a);
+        }
+
+        public bool Delete(int id)
+        {
+            return pers.Delete(id);
+        }
+
+        public List<Asistente> BuscarAsistentePorEdad(short edad)
+        {
+            return pers.BuscarAsistentePorEdad(edad);
+        }
+
+        public List<Asistente> BuscarAsistentePorTurno(string turno)
+        {
+            return pers.BuscarAsistentePorTurno(turno);
+        }
+
+        public int EliminarAsistentesDeEdadEspecifica(short edad)
+        {
+            return pers.EliminarAsistentesDeEdadEspecifica(edad);
+        }
+    }
+}
---
+++ PNT_TP1_GRUPO4/Program.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP1_GRUPO4.Persistencia;
+using TP1_GRUPO4.Negocios;
+using TP1_GRUPO4.Negocios.Actividades;
+using TP1_GRUPO4.Negocios.Entidades;
+
+namespace TP1_GRUPO4
+{
+    internal class Program
+    {
+        static Actividad_asistente actividad = new Actividad_asistente();
+        static void Main()
+        {
+            Console.Clear();//AGREGADO
+            Console.WriteLine("MANEJO DE ASISTENTES");
+            Console.WriteLine("-------");
+            Console.WriteLine("1 - Alta Asistente");
+            Console.WriteLine("2 - Modificar Asistente");//ARREGLAR UPDATE... ERROR SI PONES UN ID QUE NO EXISTE
+            Console.WriteLine("3 - Buscar Asistente por ID");
+            Console.WriteLine("4 - Listar a todos los Asistentes");
+            Console.WriteLine("5 - Borrar Asistente");
+            Console.WriteLine("6 - Buscar Asistente por edad"); // Funciona
+            Console.WriteLine("7 - Buscar Asistente por turno"); // Funciona
+            Console.WriteLine("8 - Eliminar Asistentes de una edad especifica");
+            Console.Write("Ingrese una de las opciones: ");
+
+            short sel = Convert.ToInt16(Console.ReadLine());
+
+            switch (sel)
+            {
+                case 1:
+                    {
+                        Crear();
+                        break;
+                    }
+                case 2:
+                    {
+                        Update();
+                        break;
+                    }
+                case 3:
+                    {
+                        Read();
+                        break;
+                    }
+                case 4:
+                    {
+                        ReadAll();
+                        break;
+                    }
+                case 5:
+                    {
+                        Delete();
+                        break;
+                    }
+                case 6:
+                    {
+                        BuscarAsistentePorEdad();
+                        break;
+                    }
+                case 7:
+                    {
+                        BuscarAsistentePorTurno();
+                        break;
+                    }
+                case 8:
+                    {
+                        EliminarAsistentesDeEdadEspecifica();
+                        break;
+                    }
+            }
+
+        }
+
+        static void EliminarAsistentesDeEdadEspecifica()
+        {
+            Console.Clear();
+            Console.WriteLine("Borrar Asistentes con edad especificada");
+
+            Console.WriteLine("Ingrese la edad necesitada: ");
+            short edad = short.Parse(Console.ReadLine());
+
+            int eliminados = actividad.EliminarAsistentesDeEdadEspecifica(edad);
+
+            if (eliminados > 0)
+            {
+                Console.WriteLine(eliminados + " Asistente eliminado");
+            }
+            else
+            {
+                Console.WriteLine("Ningun Asistente fue eliminado");
+            }
+
+            Console.ReadKey();
+            Main();
+        }
+
+        static void Crear()
+        {
+            Console.Clear();
+            Console.WriteLine("Agregar Asistente");
+
+            Asistente asistente = new Asistente();
+            Console.WriteLine("Ingrese su id: ");
+            asistente.id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese su nombre: ");
+            asistente.nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su apellido: ");
+            asistente.apellido = Console.ReadLine();
+            Console.WriteLine("Ingrese su edad: ");
+            asistente.edad = short.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese su turno: ");
+            asistente.turno = Console.ReadLine();
+            Console.WriteLine("Ingrese su descripcion: ");
+            asistente.descripcion = Console.ReadLine();
+
+
+            bool estado = actividad.Create(asistente);
+
+            if (estado)
+            {
+                Console.WriteLine("Se guardo correctamente al Asistente");
+            }
+            else
+            {
+                Console.WriteLine("No pudo guardarse al Asistente");
+            }
+
+            Console.ReadKey();
+            Main();
+
+        }
+
+       static void Update() // <-------- TIRA ERROR AL MODIFICAR UN ASISTENTE "INEXISTENTE"
+        {
+            Console.Clear();
+            Console.WriteLine("Actualizar Asistente");
+            Asistente asistente = new Asistente();
+            Console.WriteLine("Ingrese su id: ");
+            asistente.id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese su nombre: ");
+            asistente.nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su apellido: ");
+            asistente.apellido = Console.ReadLine();
+            Console.WriteLine("Ingrese su edad: ");
+            asistente.edad = short.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese su turno: ");
+            asistente.turno = Console.ReadLine();
+            Console.WriteLine("Ingrese su descripcion: ");
+            asistente.descripcion = Console.ReadLine();
+
+            //Console.WriteLine("Asistencia NOMBRE UPDATE: " + asistente.nombre);
+
+            bool actualizado = actividad.Update(asistente);
+
+            if (actualizado)
+            {
+                Console.WriteLine("Asistente actualizado");
+            }
+            else
+            {
+                Console.WriteLine("Asistente no actualizado");
+            }
+
+            Console.ReadKey();
+            Main();
+
+        }
+
+        static void Read()
+        {
+            Console.Clear();
+            Console.WriteLine("Leer Asistente");
+            Console.WriteLine("Ingrese su id: ");
+            int id = Convert.ToInt16(Console.ReadLine());
+
+            Asistente a = actividad.Read(id);
+
+            Console.WriteLine("Detalles del asistente: "); 
+
+            if (a != null)
+            {
+                Console.WriteLine("ID: " + $"{a.id}");
+                Console.WriteLine("Nombre: " + $"{a.nombre}");
+                Console.WriteLine("Apellido: " + $"{a.apellido}");
+                Console.WriteLine("Edad: " + $"{a.edad}");
+                Console.WriteLine("Turno: " + $"{a.turno}");
+                Console.WriteLine("Descripcion: " + $"{a.descripcion}");
+            }
+            else
+            {
+
+                Console.WriteLine("No se encontro el Asistente con id " + $"{id}");
+
+            }
+
+            Console.ReadKey();
+            Main();
+
+        }
+
+        static void ReadAll()
+        {
+            Console.Clear();
+            Console.WriteLine("Asistentes: ");
+
+            List<Asistente> asistente = actividad.ReadAll();
+
+            //Console.WriteLine("ASISTENTES EN READ ALL " + asistente.Count);
+
+            if (asistente.Count != 0)
+            {
+                foreach (var a in asistente)
+                {
+                    Console.WriteLine("id " + $"{a.id}");
+                    Console.WriteLine("Nombre " + $"{a.nombre}");
+                    Console.WriteLine("Apellido " + $"{a.apellido}");
+                    Console.WriteLine("Edad " + $"{a.edad}");
+                    Console.WriteLine("Turno " + $"{a.turno}");
+                    Console.WriteLine("Descripcion " + $"{a.descripcion}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No se encontraron Asistentes");
+            }
+
+            Console.ReadKey();
+            Main();
+
+        }
+
+        static void Delete()
+        {
+            Console.Clear();
+            Console.WriteLine("Borrar Asistente");
+
+            Console.WriteLine("Ingrese su id: ");
+            int id = int.Parse(Console.ReadLine());
+
+            bool eliminado = actividad.Delete(id);
+
+            if (eliminado)
+            {
+                Console.WriteLine("Asistente eliminado");
+            }
+            else
+            {
+                Console.WriteLine("Asistente no eliminado");
+            }
+
+            Console.ReadKey();
+            Main();
+
+        }
+
+        static void BuscarAsistentePorEdad()
+        {
+            Console.Clear();
+            Console.WriteLine("Buscando Asistentes por Edad");
+
+            Console.WriteLine("Ingrese la edad buscada: ");
+            short edad = short.Parse(Console.ReadLine());
+
+            List<Asistente> buscados = actividad.BuscarAsistentePorEdad(edad);
+
+            if (buscados.Count() > 0)
+            {
+                Console.WriteLine("Se encontraron " + buscados.Count() + " Asistentes con la edad solicitada");
+            }
+            else
+            {
+                Console.WriteLine("No se encontraron Asistentes con esa edad ");
+            }
+
+            Console.ReadKey();
+            Main();
+
+        }
+
+        static void BuscarAsistentePorTurno()
+        {
+            Console.Clear();
+            Console.WriteLine("Buscando Asistentes por Turno");
+
+            Console.WriteLine("Ingrese el turno buscado: ");
+            string turno = Console.ReadLine();
+
+            List<Asistente> buscados = actividad.BuscarAsistentePorTurno(turno);
+
+            if (buscados.Count() > 0)
+            {
+                Console.WriteLine("Se encontraron " + buscados.Count() + " Asistentes en el turno solicitado");
+            }
+            else
+            {
+                Console.WriteLine("No se encontraron Asistentes en el turno solicitado ");
+            }
+
+            Console.ReadKey();
+            Main();
+        }
+    }
+}
